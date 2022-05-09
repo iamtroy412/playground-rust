7cfc36154cd6f3296fdd379cef144262fc824f01
Troy Caro <65050284+iamtroy412@users.noreply.github.com>
2022-05-09 10:59:12 -0400

    Struct, ownership

--- ch4/ownership_moves/src/main.rs
+++ ch4/ownership_moves/src/main.rs
@@ -15,4 +15,28 @@ fn main() {
     let point = Box::new((0.625, 0.5));
     let label = format!("{:?}", point);
     assert_eq!(label, "(0.625, 0.5)");
+
+    // Just as variables own their values, structs own their fields, and tuples,
+    // arrays, and vectors own their elements.
+    struct Person {
+        name: String,
+        birth: i32,
+    }
+
+    let mut composers = Vec::new();
+    composers.push(Person {
+        name: "Palestrina".to_string(),
+        birth: 1525,
+    });
+    composers.push(Person {
+        name: "Dowland".to_string(),
+        birth: 1563,
+    });
+    composers.push(Person {
+        name: "Lully".to_string(),
+        birth: 1632,
+    });
+    for composer in &composers {
+        println!("{}, born {}", composer.name, composer.birth);
+    }
 }
