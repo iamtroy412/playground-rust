3038bb215ea633e1551b649a460add8f01b8ff45
Troy Caro <65050284+iamtroy412@users.noreply.github.com>
2022-05-10 08:56:46 -0400

    References to values

--- ch4/ownership_moves/src/main.rs
+++ ch4/ownership_moves/src/main.rs
@@ -81,4 +81,52 @@ fn main() {
     let l = Label { number: 42 };
     print(l);
     println!("My label number is: {}", l.number);
+
+    // Hash table that maps String values to a vector of Vec<String>
+    use std::collections::HashMap;
+    type Table = HashMap<String, Vec<String>>;
+
+    fn show(table: &Table) {
+        for (artist, works) in table {
+            println!("works by {}:", artist);
+            for work in works {
+                println!(" {}", work);
+            }
+        }
+    }
+
+    let mut table = Table::new();
+    table.insert(
+        "Gesualdo".to_string(),
+        vec![
+            "many madrigals".to_string(),
+            "Tenebrae Responsoria".to_string(),
+        ],
+    );
+    table.insert(
+        "Caravaggio".to_string(),
+        vec![
+            "The Musicians".to_string(),
+            "The Calling of St. Matthew".to_string(),
+        ],
+    );
+    table.insert(
+        "Cellini".to_string(),
+        vec![
+            "Perseus with the head of Medusa".to_string(),
+            "a salt cellar".to_string(),
+        ],
+    );
+
+    show(&table);
+    assert_eq!(table["Gesualdo"][0], "many madrigals");
+
+    fn sort_works(table: &mut Table) {
+        for (_artist, works) in table {
+            works.sort();
+        }
+    }
+
+    sort_works(&mut table);
+    show(&table);
 }
