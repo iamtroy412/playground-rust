1bcef7f8796fa08dd8908f2cfe1af77629043036
Troy Caro <65050284+iamtroy412@users.noreply.github.com>
2022-05-11 20:04:23 -0400

    Binary tree add function

--- ch10/enum_patterns/src/main.rs
+++ ch10/enum_patterns/src/main.rs
@@ -113,4 +113,42 @@ fn main() {
             }
         }
     }
+
+    // An ordered collection of 'T's.
+    enum BinaryTree<T> {
+        Empty,
+        NonEmpty(Box<TreeNode<T>>),
+    }
+
+    // A part of a BinaryTree
+    struct TreeNode<T> {
+        element: T,
+        left: BinaryTree<T>,
+        right: BinaryTree<T>,
+    }
+
+    impl<T: Ord> BinaryTree<T> {
+        fn add(&mut self, value: T) {
+            match *self {
+                BinaryTree::Empty => {
+                    *self = BinaryTree::NonEmpty(Box::new(TreeNode {
+                        element: value,
+                        left: BinaryTree::Empty,
+                        right: BinaryTree::Empty,
+                    }))
+                }
+                BinaryTree::NonEmpty(ref mut node) => {
+                    if value <= node.element {
+                        node.left.add(value);
+                    } else {
+                        node.right.add(value);
+                    }
+                }
+            }
+        }
+    }
+
+    let mut tree = BinaryTree::Empty;
+    tree.add("Mercury");
+    tree.add("Venus");
 }
