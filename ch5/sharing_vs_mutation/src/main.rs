3cf5cf9c3892ac354d6c7e8fe099ef73cd25f5b0
Troy Caro <iamtroy412@gmail.com>
2022-05-16 19:47:29 -0400

    dangling pointer

--- ch5/sharing_vs_mutation/src/main.rs
+++ ch5/sharing_vs_mutation/src/main.rs
@@ -1,6 +1,8 @@
 fn main() {
     let v = vec![4, 8, 19, 27, 34, 10];
-    let r = &v;
-    let aside = v; // move vector to aside
-    r[0]; // bad; uses 'v', which is uninitialized.
+    {
+        let r = &v;
+        r[0]; // ok; vector is still there
+    }
+    let aside = v;
 }
