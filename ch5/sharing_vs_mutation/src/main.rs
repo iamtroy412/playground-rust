782c3580ec7e53369c990f7567839e9a22bda6e3
Troy Caro <iamtroy412@gmail.com>
2022-05-16 20:37:05 -0400

    modifying collections while pointing into them

--- ch5/sharing_vs_mutation/src/main.rs
+++ ch5/sharing_vs_mutation/src/main.rs
@@ -5,4 +5,28 @@ fn main() {
         r[0]; // ok; vector is still there
     }
     let aside = v;
+
+    fn extend(vec: &mut Vec<f64>, slice: &[f64]) {
+        for ele in slice {
+            vec.push(*ele);
+        }
+    }
+
+    let mut wave = Vec::new();
+    let head = vec![0.0, 1.0];
+    let tail = [0.0, -1.0];
+
+    extend(&mut wave, &head); // extend wave with another vector
+    extend(&mut wave, &tail); // extend wave with an array
+
+    assert_eq!(wave, vec![0.0, 1.0, 0.0, -1.0]);
+
+    // Doesn't work! pg 125. When extending &mut wave, it needs to
+    // allocate a larger buffer, but &wave is still pointing to the
+    // old buffer, which has been dropped by allocating the new one
+    // for extending. The Rust compiler catches this with a borrow
+    // error; cannot borrow mut and and immutable ref at the same time.
+    // ie one read/write or many readers.
+    extend(&mut wave, &wave);
+    assert_eq!(wave, vec![0.0, 1.0, 0.0, -1.0]);
 }
