b706ca7b204fe94db4e8ec811e1ff05fd02f1322
Troy Caro <iamtroy412@gmail.com>
2022-05-21 14:22:28 -0400

    loops

--- ch6/expressions/src/main.rs
+++ ch6/expressions/src/main.rs
@@ -85,4 +85,107 @@ fn main() {
         pattern => block1,
         _ => block2,
     }
+
+    // There are 4 looping expressions:
+    while condition {
+        block
+    }
+
+    while let pattern = expr {
+        block
+    }
+
+    loop {
+        block
+    }
+
+    for pattern in iterable {
+        block
+    }
+
+    // A for loop evaluates the iterable expression and then evaluates
+    // the block once for each value in the resulting iterator.
+    for i in 0..20 {
+        println!("{}", i);
+    }
+
+    // In keeping with Rust's move semantics, a for loop over a value
+    // consumes the value:
+    let strings: Vec<String> = error_messages();
+    for s in strings {
+        // each String is moved into 's' here...
+        println!("{}", s);
+    } // ... and dropped here.
+    println!("{} error(s)", strings.len()); // error: use of moved value
+
+    // This can be inconvenient. The easy fix is to loop over a reference
+    // to the collection instead. The loop variable then, will be a reference
+    // to each item in the collection:
+    for rs in &strings {
+        println!("String {:? is at address {:p}.", *rs, rs);
+    }
+    // Here the type of &strings is &Vev<String>, and the type of
+    // rs is &String.
+
+    // Iterating over a mut refernce provides a mut reference to each element.
+    for rs in &mut strings {
+        // the type of rs is &mut String
+        rs.push('\n'); // add a newline to each string
+    }
+
+    // A break expression exits an enclosing loop. Withinin the body of a loop,
+    // you can give break an expression, whose value becomes that of the loop.
+    // Naturally, all the break expressions within a loop must produce values
+    // with the same type, which becomes the type of the loop itself.
+
+    // Each call to 'next_line' returns either 'Some(line)', where 'line'
+    // is a line of input, or 'None', if we've reached the end of the input.
+    // Return the first line that starts with "answer: ".
+    // Otherwise, return "answer: nothing".
+    let answer = loop {
+        if let Some(line) = next_line() {
+            if line.starts_with("answer: ") {
+                break line;
+            }
+        } else {
+            break "answer: nothing";
+        }
+    };
+
+    // A continue expression jumps to the next loop iteration:
+    for line in input_lines {
+        let trimmed = trim_comments_and_whitespace(line);
+        if trimmed.is_empty() {
+            // Jump back to the top of the loop and
+            // move on to the next line of input.
+            continue;
+        }
+    }
+
+    // A loop can be labeled with a lifetime. In the following example,
+    // 'search: is a label for the outter loop. Thus, break 'search
+    // exits that loop, not the inner loop:
+    'search: for room in apartment {
+        for spot in room.hiding_spots() {
+            if spot.contains(keys) {
+                println!("Your keys are {} in the {}.", spot, room);
+                break 'search;
+            }
+        }
+    }
+
+    // A break can have both a label and a value expression:
+    let sqrt = 'outer: loop {
+        let n = next_number();
+        for i in 1.. {
+            let square = i * i;
+            if square == n {
+                break 'outer i;
+            }
+            if square > n {
+                break;
+            }
+        }
+    };
+    // Labels can also be used with continue.
 }
